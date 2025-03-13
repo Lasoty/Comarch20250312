a521c8051afb311ae66377b7d2bdeb651aaa38ee
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-03-13 10:57:43 +0100

    Parametry wyjściowe

--- ComarchCwiczenia20250312.App/Program.cs
+++ ComarchCwiczenia20250312.App/Program.cs
@@ -17,28 +17,27 @@ namespace ComarchCwiczenia20250312.App
 
             if (czyPoprawnyWybor)
             {
-                Console.Write("Podaj X: ");
-                int x = int.Parse(Console.ReadLine());
-
-                Console.Write("Podaj Y: ");
-                int y = int.Parse(Console.ReadLine());
-
+                int x, y;
 
                 switch (wybor)
                 {
                     case 1:
+                        GetXY(out x, out y);
                         Console.WriteLine($"Wynik dodawania {x} oraz {y} to {kalkulator.Add(x, y)}.");
                         //Console.WriteLine("Wynik dodawania " + x + " oraz " + y + " to " + x + y + ".");
                         //Console.WriteLine("Wynik dodawania {0} oraz {1} to {2}.", x, y, x + y);
                         //Console.WriteLine(string.Format("Wynik dodawania {0} oraz {1} to {2}.", x, y, x + y));
                         break;
                     case 2:
+                        GetXY(out x, out y);
                         Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {kalkulator.Subtract(x, y)}.");
                         break;
                     case 3:
+                        GetXY(out x, out y);
                         Console.WriteLine($"Wynik mnożenie {x} oraz {y} to {kalkulator.Multiply(x, y)}.");
                         break;
                     case 4:
+                        GetXY(out x, out y);
                         Console.WriteLine($"Wynik dzielenie {x} oraz {y} to {kalkulator.Divide(x, y)}.");
                         break;
                     default:
@@ -50,6 +49,14 @@ namespace ComarchCwiczenia20250312.App
             {
                 ShowError("Podana wartość jest nieprawidłowa");
             }
+        }
+
+        private static void GetXY(out int x, out int y)
+        {
+            Console.Write("Podaj X: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Podaj Y: ");
+            y = int.Parse(Console.ReadLine());
         }
 
         private static void ShowMenu()
