9400946c6384fd9f325b1cf7dd7a3f434f532cc9
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-03-13 10:38:20 +0100

    Metody i testy jednostkowe

--- ComarchCwiczenia20250312.App/Program.cs
+++ ComarchCwiczenia20250312.App/Program.cs
@@ -1,19 +1,19 @@
-﻿namespace ComarchCwiczenia20250312.App
+﻿using ComarchCwiczenia20250312.App.Cwiczenia;
+
+namespace ComarchCwiczenia20250312.App
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("KALKULATOR 1.0");
-            Console.WriteLine(" 1. Dodawanie");
-            Console.WriteLine(" 2. Odejmowanie");
-            Console.WriteLine(" 3. Mnożenie");
-            Console.WriteLine(" 4. Dzielenie");
+            ShowMenu();
 
             Console.Write("Wybierz opcję: ");
             string sWybor = Console.ReadLine();
             int wybor;
             bool czyPoprawnyWybor = int.TryParse(sWybor, out wybor);
+
+            Kalkulator kalkulator = new Kalkulator();
 
             if (czyPoprawnyWybor)
             {
@@ -27,33 +27,45 @@
                 switch (wybor)
                 {
                     case 1:
-                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {x + y}.");
+                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {kalkulator.Add(x, y)}.");
                         //Console.WriteLine("Wynik dodawania " + x + " oraz " + y + " to " + x + y + ".");
                         //Console.WriteLine("Wynik dodawania {0} oraz {1} to {2}.", x, y, x + y);
                         //Console.WriteLine(string.Format("Wynik dodawania {0} oraz {1} to {2}.", x, y, x + y));
                         break;
                     case 2:
-                        Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {x - y}.");
+                        Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {kalkulator.Subtract(x, y)}.");
                         break;
                     case 3:
-                        Console.WriteLine($"Wynik mnożenie {x} oraz {y} to {x * y}.");
+                        Console.WriteLine($"Wynik mnożenie {x} oraz {y} to {kalkulator.Multiply(x, y)}.");
                         break;
                     case 4:
-                        Console.WriteLine($"Wynik dzielenie {x} oraz {y} to {x / y}.");
+                        Console.WriteLine($"Wynik dzielenie {x} oraz {y} to {kalkulator.Divide(x, y)}.");
                         break;
                     default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Podana wartość jest spoza zakresu menu");
-                        Console.ResetColor();
+                        ShowError("Podana wartość jest spoza zakresu menu");
                         break;
                 }
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Podana wartość jest nieprawidłowa");
-                Console.ResetColor();
+                ShowError("Podana wartość jest nieprawidłowa");
             }
-         }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("KALKULATOR 1.0");
+            Console.WriteLine(" 1. Dodawanie");
+            Console.WriteLine(" 2. Odejmowanie");
+            Console.WriteLine(" 3. Mnożenie");
+            Console.WriteLine(" 4. Dzielenie");
+        }
+
+        private static void ShowError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
     }
 }
---
+++ Tests/ComarchCwiczenia20250312.Unit.Tests/App/Cwiczenia/KalkulatorTests.cs
@@ -0,0 +1,30 @@
+﻿using ComarchCwiczenia20250312.App.Cwiczenia;
+
+namespace ComarchCwiczenia20250312.Unit.Tests.App.Cwiczenia;
+
+[TestFixture]
+public class KalkulatorTests
+{
+    private Kalkulator cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new Kalkulator();
+    }
+
+    [Test]
+    public void Add_Should_ReturnValidSum()
+    {
+        //Arrange
+        int x = 2;
+        int y = 3;
+        int expected = 5;
+
+        // Act
+        int actual = cut.Add(x, y);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
