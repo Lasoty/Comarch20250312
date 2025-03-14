23ab9c92d377cb4f6eb890f7d9db5c72e3ff4a95
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-03-14 10:58:11 +0100

    Konfiguracja Swagger

--- Bibliotekarz/Bibliotekarz.Api/Program.cs
+++ Bibliotekarz/Bibliotekarz.Api/Program.cs
@@ -1,34 +1,42 @@
+namespace Bibliotekarz.Api;
 
-namespace Bibliotekarz.Api
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        // Konfiguracja Swaggera
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        // Use CORS policy
+        app.UseCors("AllowAll");
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
